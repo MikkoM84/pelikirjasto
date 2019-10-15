25c0b8542e5856761ca7eb797c952471f8c729dc
MikkoM84 <mikkom84@yahoo.com>
2019-10-15 21:16:25 +0300

    reset

--- source/backend/models/session.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-let Schema = mongoose.Schema({
-	username:String,
-	token:String,
-	ttl:Number //timetolive
-});
-
-module.exports = mongoose.model("Session",Schema);
