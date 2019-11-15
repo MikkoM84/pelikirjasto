c788c893a677470d4f491c58a5bb4debf9b1f0b5
MikkoM84 <mikkom84@yahoo.com>
2019-11-15 16:20:06 +0200

    muuttujien ja tiedostojen uudelleen nimeämistä

--- source/backend/models/kategoriatItem.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-let Schema = mongoose.Schema({
-	kategoria:String,
-	username:{type:String,indexed:true}
-});
-
-module.exports = mongoose.model("KategoriatItem",Schema);
--- source/backend/models/kokoelmatItem.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-let Schema = mongoose.Schema({
-	kokoelma:String,
-	username:{type:String,indexed:true}
-});
-
-module.exports = mongoose.model("KokoelmatItem",Schema);
